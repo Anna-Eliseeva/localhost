0198dcf47f9421aa0bc0dc0393099fdc2dc7f167
Anna-Eliseeva <eliseeva9797@mail.ru>
2016-10-09 23:34:04 +0300

    Повтор массивов

--- spora.php
+++ spora.php
@@ -7,8 +7,48 @@
 
 //$str2 = 'lalalal 5454';
 
-
 //$str2 = <<<HERE
 //lalalal 5454
-HERE;
-//echo $str2;
+//HERE;
+//echo $str2;kk
+//$i = 1;
+//echo '<table border = "1">';
+//while($n <= 3) {
+//    echo "<td>Row$i | Col - $n </td>";
+//    $n++;
+//}
+//echo '</table>';
+//$year = 1900;
+//echo '<select>';
+//while($year <= 2016) {
+//    echo "<option value = '$year'>$year </option>";
+//    $year++;
+//}
+//echo '</select>';
+//$x = 1;
+//echo '<table border = "1">';
+//while($x < 10) {
+//    $x++;
+//    echo "<tr>";
+//    $y = 1;
+//    while($y < 10) {
+//        echo "<td>$x * $y = " . $x * $y. "</td>";
+//        $y++;
+//    }
+//    echo "<tr>";
+//}
+//Таблица умножения
+//echo '</table>';
+
+$a = array('second','third','first');
+if(2 > 0){
+    echo 'first';
+
+}
+if(1 > 0){
+    echo 'second';
+}
+if(2 > 0){
+    echo 'third';
+}
+
