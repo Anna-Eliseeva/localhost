8696062a729093c37ed6cab074af60e8ba06bc25
Anna-Eliseeva <eliseeva9797@mail.ru>
2016-10-12 22:28:15 +0300

    начинаем писать базу данных

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+
+    require __DIR__ .  "/libs/rb.php";
+
+?>
+ <form action="auth.php" method="POST"></form>
+<p>
+    <strong>Ваш логин</strong>
+    <input type="text" name="login">
+</p>
+
+
+
